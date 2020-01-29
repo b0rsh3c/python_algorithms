f1a00265be428b19e35c6be31160e5a1fa526268
b0rsh3c <vanya-pro93@yandex.ru>
2020-01-29 13:12:10 +0300

    lesson7
    непонятен момент с "По возможности доработайте алгоритм (сделайте его умнее)"

---
+++ Lesson_7/1.py
@@ -0,0 +1,23 @@
+"""
+1. Отсортируйте по убыванию методом "пузырька" одномерный целочисленный массив,
+заданный случайными числами на промежутке [-100; 100). Выведите на экран
+исходный и отсортированный массивы. Сортировка должна быть реализована в
+виде функции. По возможности доработайте алгоритм (сделайте его умнее).
+"""
+import random
+r = [random.randint(-100, 100) for _ in range(30)]
+print(r)
+
+def bubble_sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        flag = True
+        for n in range(i):
+            if array[n] > array[n+1]:
+                array[n], array[n+1] = array[n+1], array[n]
+                flag = False
+
+        if flag == True:
+            break
+    return array
+
+print(bubble_sort(r))
